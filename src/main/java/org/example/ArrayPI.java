cf8f0ea00aa6ab99d49b4653bc73c7d9b931597f
Annushkkka <anyuta260703@gmail.com>
2024-10-08 16:20:29 +0300

    Размер массива был изменен с 5 до 35 для соблюдения требований по задаче

--- src/main/java/org/example/ArrayPI.java
+++ src/main/java/org/example/ArrayPI.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ArrayPI {
     // Одномерный массив с размером 5
-    private int[] array = new int[5];
+    private int[] array = new int[35];
 
     // Метод для ввода массива с клавиатуры
     public void inputArray() {
@@ -12,7 +12,9 @@ public class ArrayPI {
         boolean isValidInput;
 
         do {
-            System.out.println("Введите 5 целых чисел, разделенных пробелами или нажимая Enter после каждого (например: 1 2 3 4 5):");
+            System.out.println("Введите 35 целых чисел, разделенных пробелами или нажимая Enter после каждого");
+            System.out.println("(например: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35):");
+
 
             // Ввод через пробел
             String input = scanner.nextLine(); // Считываем всю строку
@@ -24,7 +26,7 @@ public class ArrayPI {
             // Преобразуем строковые значения в числа и сохраняем их в массив
             for (String number : numbers) {
                 try {
-                    if (count < 5) {
+                    if (count < 35) {
                         array[count++] = Integer.parseInt(number);
                     }
                 } catch (NumberFormatException e) {
@@ -34,7 +36,7 @@ public class ArrayPI {
             }
 
             // Если пользователь не ввел все 5 чисел, можно продолжить ввод построчно
-            while (count < 5 && isValidInput) {
+            while (count < 35 && isValidInput) {
                 System.out.println("Введите число " + (count + 1) + ":");
                 try {
                     array[count++] = scanner.nextInt(); // Ввод числа
